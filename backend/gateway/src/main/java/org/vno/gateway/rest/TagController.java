0c2a06930e6ab297da96c3e1448492c19fa2fc84
Valeriy Kireev <valeriykireev@gmail.com>
2018-04-13 22:01:45 +0300

    Fix tag controller mapping

--- backend/gateway/src/main/java/org/vno/gateway/rest/TagController.java
+++ backend/gateway/src/main/java/org/vno/gateway/rest/TagController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.vno.gateway.bridge.MongoBridge;
@@ -18,7 +17,7 @@ import org.vno.gateway.domain.Tag;
 /**
  * @author kk
  */
-@RequestMapping("/ref")
+@RequestMapping("/tag")
 @RestController
 @PreAuthorize("isAuthenticated()")
 public class TagController {
