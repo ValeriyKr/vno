a844ba8f9aaf484c84e03e82197ea9fddc1e252b
Valeriy Kireev <valeriykireev@gmail.com>
2018-05-25 23:04:18 +0300

    Add revision controller

--- backend/pg/src/main/java/org/vno/pg/repository/CommitRepository.java
+++ backend/pg/src/main/java/org/vno/pg/repository/CommitRepository.java
@@ -3,8 +3,11 @@ package org.vno.pg.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.vno.pg.domain.Commit;
 
+import java.util.Collection;
+
 /**
  * @author kk
  */
 public interface CommitRepository extends CrudRepository<Commit, Long> {
+    Collection<Commit> findAll(Iterable<Long> ids);
 }
