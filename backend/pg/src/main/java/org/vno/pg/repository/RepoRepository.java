dbb7ccbbf0c8fb008935cdb3d23df7140ce59cd8
Valeriy Kireev <valeriykireev@gmail.com>
2018-05-25 21:09:36 +0300

    Add tag controller

--- backend/pg/src/main/java/org/vno/pg/repository/RepoRepository.java
+++ backend/pg/src/main/java/org/vno/pg/repository/RepoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.vno.pg.domain.Branch;
 import org.vno.pg.domain.Repo;
+import org.vno.pg.domain.Tag;
 
 /**
  * @author kk
@@ -14,4 +15,9 @@ public interface RepoRepository extends CrudRepository<Repo, Long> {
     Repo findOneWithBranches(@Param("id") Long id);
 
     Repo findByBranchesContains(Branch b);
+
+    @Query("SELECT r FROM Repo r LEFT JOIN FETCH r.tags WHERE r.id = :id")
+    Repo findOneWithTags(@Param("id") Long id);
+
+    Repo findByTagsContains(Tag t);
 }
